a62c3dfb1b29dbcd7f52b2386b61bf7e2dd48738
Dan Fyfe <fyfedaniel@gmail.com>
2024-04-22 12:30:37 -0400

    add field for slug

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -2,6 +2,7 @@ namespace danscoconutsdotnet.Models;
 
 public class Post(string title, string markdown, string slug)
 {
+  public int PostId { get; set; }
   public required string Title { get; set; } = title;
   public required string Markdown { get; set; } = markdown;
   public required string Slug { get; set; } = slug;
